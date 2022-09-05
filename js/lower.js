a415515a0ae4624d51f7c98b1920a82352c3d04e
BoraKendir <bora.kendir@std.yeditepe.edu.tr>
2022-09-05 15:32:06 +0300

    resimler eksik haliyle ilk 3 bitti, css hala barebone

--- js/lower.js
+++ js/lower.js
@@ -1,7 +1,12 @@
 function picturecontainertoggle(){
     const picturecontainer = document.getElementById('LordResimler');
     if(picturecontainer.style.display == 'none'){
-        picturecontainer.style.display = "flex"; 
+        picturecontainer.style.display = "flex";
+        picturecontainer.style.flexDirection = "row";
+        picturecontainer.style.flexWrap = "wrap";
+        picturecontainer.style.alignContent = "center";
+        picturecontainer.style.justifyContent = "center";
+        picturecontainer.style.alignItems = "center"; 
     } 
     else if(picturecontainer.style.display === "flex"){
         picturecontainer.style.display = "none";
