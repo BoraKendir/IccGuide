cf75571088bbc28e8480c8464c96d8eed69bf464
BoraKendir <bora.kendir@std.yeditepe.edu.tr>
2022-09-08 15:27:22 +0300

    Lower Spire barebone bitti, resimler yüklendi, css styling sonra yapılabilir

--- js/lower.js
+++ js/lower.js
@@ -1,14 +1,29 @@
-function picturecontainertoggle(){
-    const picturecontainer = document.getElementById('LordResimler');
-    if(picturecontainer.style.display == 'none'){
-        picturecontainer.style.display = "flex";
-        picturecontainer.style.flexDirection = "row";
-        picturecontainer.style.flexWrap = "wrap";
-        picturecontainer.style.alignContent = "center";
-        picturecontainer.style.justifyContent = "center";
-        picturecontainer.style.alignItems = "center"; 
-    } 
-    else if(picturecontainer.style.display === "flex"){
-        picturecontainer.style.display = "none";
+function picturecontainertoggle(difficulty){
+    if(difficulty === "NM"){
+        const picturecontainer = document.getElementById('LordResimlerNM');
+        if(picturecontainer.style.display == 'none'){
+            picturecontainer.style.display = "flex";
+            picturecontainer.style.flexDirection = "row";
+            picturecontainer.style.flexWrap = "wrap";
+            picturecontainer.style.alignContent = "center";
+            picturecontainer.style.justifyContent = "center";
+            picturecontainer.style.alignItems = "center"; 
+        } 
+        else if(picturecontainer.style.display === "flex"){
+            picturecontainer.style.display = "none";
+        }
+    } else if(difficulty === 'HC'){
+        const picturecontainer = document.getElementById('LordResimlerHC');
+        if(picturecontainer.style.display == 'none'){
+            picturecontainer.style.display = "flex";
+            picturecontainer.style.flexDirection = "row";
+            picturecontainer.style.flexWrap = "wrap";
+            picturecontainer.style.alignContent = "center";
+            picturecontainer.style.justifyContent = "center";
+            picturecontainer.style.alignItems = "center"; 
+        } 
+        else if(picturecontainer.style.display === "flex"){
+            picturecontainer.style.display = "none";
+        }
     }
 }
